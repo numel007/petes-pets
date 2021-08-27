9762fc55e0aa2d43ce9193e92075cc43f0a85445
Jackie <taekwondo.chan@gmail.com>
2021-08-26 17:32:56 -0700

    cloned and commented

--- server.js
+++ server.js
@@ -1,62 +1,66 @@
 if (!process.env.PORT) {
-  require('dotenv').config()
-  process.env.NODE_ENV = "dev"
+	require("dotenv").config();
+	process.env.NODE_ENV = "dev";
 }
 
-const express = require('express');
-const path = require('path');
-const favicon = require('serve-favicon');
-const logger = require('morgan');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override')
+const express = require("express");
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const methodOverride = require("method-override");
 
 const app = express();
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/local', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/local", {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true,
+	useFindAndModify: false,
 });
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "pug");
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // override with POST having ?_method=DELETE or ?_method=PUT
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+// Logger middleware, uses dev formatting
+app.use(logger("dev"));
+
+// Reads urls and json content-type and returns as req.body
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Decodes cookies using a secret
 app.use(cookieParser());
 
-
-require('./routes/index.js')(app);
-require('./routes/pets.js')(app);
+require("./routes/index.js")(app);
+require("./routes/pets.js")(app);
 
 // catch 404 and forward to error handler
 app.use((req, res, next) => {
-  const err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	const err = new Error("Not Found");
+	err.status = 404;
+	next(err);
 });
 
 // error handler
 app.use((err, req, res, next) => {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+	// set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+	// render the error page
+	res.status(err.status || 500);
+	res.render("error");
 });
 
 module.exports = app;
